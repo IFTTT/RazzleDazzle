b1952efc63330f2e76acd0f3f2418b4931ef5525
Eduardo Irías <eduardoirias@me.com>
2020-01-27 02:03:14 +0100

    Add Carthage Support

--- Sources/RazzleDazzle/RazzleDazzle.h
+++ Sources/RazzleDazzle/RazzleDazzle.h
@@ -6,10 +6,12 @@
 //  Copyright (c) 2015 IFTTT. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for RazzleDazzle.
 FOUNDATION_EXPORT double RazzleDazzleVersionNumber;
 
 //! Project version string for RazzleDazzle.
 FOUNDATION_EXPORT const unsigned char RazzleDazzleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RazzleDazzle/PublicHeader.h>
